32c172847f968ab0cae36d7752c0d768a3bfe5a0
joaquin solis <sol19008@byui.edu>
2023-02-06 23:50:23 -0300

    add content array

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,45 @@
 import Layer from "./components/Layer";
 import LineText from "./components/Line";
 
+const content = [
+  {
+    title: "Pure CSS",
+    subtitle: "Tailwind Scroll",
+    background: "bg-green-500 bg-opacity-10",
+    id: 1,
+  },
+  {
+    title: "Animate",
+    subtitle: "Anything",
+    background: "bg-pink-500 bg-opacity-10",
+    id: 2,
+  },
+  {
+    title: "Pure CSS",
+    subtitle: "Tailwind Scroll",
+    background: "bg-green-500 bg-opacity-10",
+    id: 3,
+  },
+  {
+    title: "Animate",
+    subtitle: "Anything",
+    background: "bg-pink-500 bg-opacity-10",
+    id: 4,
+  },
+];
+
 function App() {
   return (
     <div className="group/wrap relative">
       <div className="relative h-screen snap-y snap-mandatory overflow-y-auto scroll-smooth">
-        <Layer styles={"w-full"}>
-          <LineText>Pure CSS</LineText>
-          <LineText styles={"group-hover:animation-delay-300"}>
-            Tailwind Scroll
-          </LineText>
-        </Layer>
-
-        <Layer styles={"bg-green-500 bg-opacity-10"}>
-          <LineText>Animate</LineText>
-          <LineText styles={"group-hover:animation-delay-300"}>
-            Anything
-          </LineText>
-        </Layer>
-
-        <Layer>
-          <LineText>Pure CSS</LineText>
-          <LineText styles={"group-hover:animation-delay-300"}>
-            Tailwind Scroll
-          </LineText>
-        </Layer>
-
-        <Layer styles={"bg-pink-500 bg-opacity-10"}>
-          <LineText>Animate</LineText>
-          <LineText styles={"group-hover:animation-delay-300"}>
-            Anything
-          </LineText>
-        </Layer>
+        {content.map((item) => (
+          <Layer key={item.id} styles={item.background}>
+            <LineText>{item.title}</LineText>
+            <LineText styles={"group-hover:animation-delay-300"}>
+              {item.subtitle}
+            </LineText>
+          </Layer>
+        ))}
       </div>
     </div>
   );
